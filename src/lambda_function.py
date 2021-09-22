7109d1ee1252157ff5b3b88f0c46b52dedac4f6d
n.fujita <nobuyuf@amazon.co.jp>
2021-09-22 23:36:09 +0900

    update

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -1,40 +1,22 @@
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: Apache-2.0
 
-"""
-Purpose
-Shows how to implement an AWS Lambda function that handles input from direct
-invocation.
-"""
 
+import os
+import json
 import logging
-import math
+
+# Import Bolt for Python (github.com/slackapi/bolt-python)
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-# Define a list of Python lambda functions that are called by this AWS Lambda function.
-ACTIONS = {
-    'square': lambda x: x * x,
-    'square root': lambda x: math.sqrt(x),
-    'increment': lambda x: x + 1,
-    'decrement': lambda x: x - 1,
-}
-
 
 def lambda_handler(event, context):
-    """
-    Accepts an action and a number, performs the specified action on the number,
-    and returns the result.
-    :param event: The event dict that contains the parameters sent when the function
-                  is invoked.
-    :param context: The context in which the function is called.
-    :return: The result of the specified action.
-    """
-    logger.info('Event: %s', event)
 
-    result = ACTIONS[event['action']](event['number'])
-    logger.info('Calculated result of %s', result)
 
-    response = {'result': result}
-    return response
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
